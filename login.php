e2806770d8e8c33f6e83f60959a28312bf4a501f
MishellOV <mishellov_12@hotmail.com>
2025-05-19 21:36:16 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="login-body">
+    <div class="container">
+        <h1>Login</h1>
+        <form action="home.php" method="post">
+            <input type="email" name="email" placeholder="e-mail" required>
+            <input type="password" name="senha" placeholder="senha" required>
+            <button type="submit">ENTRAR</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ perfil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Perfil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="perfil-header">
+        <a href="dashboard.php">&#8592;</a>
+        <h2>Perfil</h2>
+        <a href="#">✎</a>
+    </div>
+
+    <div class="perfil-form">
+        <label>Nome</label><input type="Maria da Silva Castro" disabled>
+        <label>CPF</label><input type="123.456.789-00" disabled>
+        <label>Data de nascimento</label><input type="18/03/1983" disabled>
+        <label>RG</label><input type="1234567-8" disabled>
+        <label>Telefone</label><input type="99990011" disabled>
+        <label>Rua</label><input type="text" disabled>
+    </div>
+</body>
+</html>
